ee842ab6950c05979891502792e2bc8d17acc468
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-07-21 23:20:59 +0900

    Time: 107 ms (16.63%), Space: 41.8 MB (88.51%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    const idx=nums.findIndex(v=>v===target);
+    if(idx!==-1){
+        return idx;
+    }else{
+        let left=0;
+        let right=nums.length-1;
+        while(left<right){
+            if(target>nums[left]){
+                left++;
+            }else if(target<nums[right]){
+                right--;
+            }
+        }
+        if(nums[nums.length-1]<target){
+            return left+1;
+        }
+        console.log(left,right)
+        return left;
+    }
+};
