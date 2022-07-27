9d484d32cd74bdf7b691744b2e7d3de43e9a984c
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-07-27 09:59:10 +0900

    Time: 302 ms (6.91%), Space: 44.9 MB (40.32%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let idx=0;
+    while(idx!==nums.length){
+            if(nums[idx-1]===nums[idx]){
+                nums.splice(idx,1)
+            }else{
+                idx++;
+            }
+         }
+};
