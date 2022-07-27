49fab1141849ab2999afe6145deb4e9ee03fa728
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-07-27 23:05:19 +0900

    Time: 92 ms (41.80%), Space: 41.7 MB (84.81%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -3,32 +3,43 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let result=[];
-    let num=digits.pop()+1;
-    let ten=false;
-    if(num>=10){
-        ten=true;
-        num=0;   
-    }
-    result.push(num);
     for(let i=digits.length-1;i>=0;i--){
-        num=digits[i];
-        if(ten){
-            num +=1;   
+        if(digits[i]!==9){
+            digits[i]+=1;
+            return digits;
         }
-        
-        if(num>=10){
-            ten=true;
-            num=0;
-        }else{
-            ten=false;
+        digits[i]=0;
+        if(i===0){
+            digits.unshift(1);
+            return digits;
         }
-        result.unshift(num);
-        
     }
+//     let result=[];
+//     let num=digits.pop()+1;
+//     let ten=false;
+//     if(num>=10){
+//         ten=true;
+//         num=0;   
+//     }
+//     result.push(num);
+//     for(let i=digits.length-1;i>=0;i--){
+//         num=digits[i];
+//         if(ten){
+//             num +=1;   
+//         }
+        
+//         if(num>=10){
+//             ten=true;
+//             num=0;
+//         }else{
+//             ten=false;
+//         }
+//         result.unshift(num);
+        
+//     }
     
-    if(ten){
-        result.unshift(1);
-    }
-    return result; 
+//     if(ten){
+//         result.unshift(1);
+//     }
+//     return result; 
 };
