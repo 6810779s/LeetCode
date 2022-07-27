c4acace99a8e4b8c07dba0e4209b429a0dfe0f50
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-07-27 12:05:46 +0900

    Time: 112 ms (15.09%), Space: 42.2 MB (33.47%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -4,12 +4,34 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    for (let i=0;i<nums.length;i++){
+    // for (let i=nums.length-1;i>-1;i--){
+    //     if(nums[i]===val){
+    //         nums.splice(i,1)
+    //     }else{
+    //         continue;
+    //     }
+    // }
+    let k=0;
+    for(let i=0;i<nums.length;i++){
         if(nums[i]===val){
-            nums.splice(i,1);
-            i-=1;
-        }else{
             continue;
+        }else{
+            if(nums[k]===val){
+                nums[k]=nums[i];
+                nums[i]=val;
+            }else{
+                nums[k]=nums[i];
+            }
+            k++;
         }
     }
+    return k;
+    // for (let i=0;i<nums.length;i++){
+    //     if(nums[i]===val){
+    //         nums.splice(i,1);
+    //         i-=1;
+    //     }else{
+    //         continue;
+    //     }
+    // }
 };
