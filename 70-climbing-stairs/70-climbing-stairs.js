6e3acfec526ad9c3d14e6c82429b3b8516b9b1b2
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-07-28 20:30:11 +0900

    Time: 96 ms (30.22%), Space: 47.9 MB (6.55%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+     let dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    console.log(dp)
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+        console.log("dp:",dp)
+    }
+    return dp[n];
+    
+    
+//     let cnt=0;
+//     function cnt_step(sum){
+//         if(sum>n){
+//              return;
+//         }
+//         if(sum===n) cnt++;
+        
+//         cnt_step(sum+1);
+//         cnt_step(sum+2);
+//     }
+    
+//     cnt_step(0);
+//     return cnt;
+};
