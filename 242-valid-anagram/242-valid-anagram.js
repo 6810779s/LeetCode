3f00b353a133d85e5a4bc9c2d6750efb88d10705
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-07-28 12:13:29 +0900

    Time: 132 ms (39.98%), Space: 45.7 MB (36.82%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,9 +4,10 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if(s.length!==t.length) return false
     const arr1=s.split("").sort();
     const arr2=t.split("").sort();
-    for(let i=0;i<Math.max(arr1.length,arr2.length);i++){
+    for(let i=0;i<arr1.length;i++){
         if(arr1[i]!==arr2[i]) return false;
     }
     return true;
