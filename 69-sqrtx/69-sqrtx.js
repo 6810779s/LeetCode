faec392fc5222d612dfe1116fc24679cbdb2e2ad
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-07-28 13:20:12 +0900

    Time: 93 ms (77.60%), Space: 44.4 MB (7.49%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -8,16 +8,16 @@ var mySqrt = function(x) {
     if(x===0||x===1) return x;
     let start=0;
     let end=parseInt(x/2)+1;
-    let num=0;
+    let mid=0;
     
     while(end-start>1){
-        num=parseInt((start+end)/2);
-        if(num*num===x){
-            return num;
-        }else if(num*num>x){
-            end=num;
-        }else if(num*num<x){
-            start=num;
+        mid=parseInt((start+end)/2);
+        if(mid*mid===x){
+            return mid;
+        }else if(mid*mid>x){
+            end=mid;
+        }else if(mid*mid<x){
+            start=mid;
         }
         
     }
