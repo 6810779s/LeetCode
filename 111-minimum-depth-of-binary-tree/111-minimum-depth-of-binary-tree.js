d5c5439de27d38e441ad37aa916a51f564c1561a
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-08-02 00:40:15 +0900

    Time: 382 ms (23.27%), Space: 100.3 MB (5.11%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -10,30 +10,42 @@
  * @param {TreeNode} root
  * @return {number}
  */
+const MAX = Math.pow(10, 5) + 1;
 
+const searchMinDepth = (root, depth) => {
+  if (root === null) return MAX;
+  depth++;
 
-var minDepth = function(root) {
-    let min=Infinity;
-    function DFS(node){
-        if(!node) return 0;
-        
-        if(!node.left) return DFS(node.right)+1;
-        if(!node.right) return DFS(node.left)+1;
-        
-        const leftDepth=DFS(node.left);
-        const rightDepth=DFS(node.right);
-        
-       
-        console.log(leftDepth,rightDepth)
-        return 1+Math.min(leftDepth,rightDepth);
-//         console.log("left:",leftDepth,"right:",rightDepth);
-//         if(min===Infinity){
-//             min=Math.min(leftDepth,rightDepth);
-//         }else{
-//             min=Math.min(min,leftDepth,rightDepth);
-//         }
-        
-//         return min;
-    }
-    return DFS(root);
+  if (root.left === null && root.right === null) return depth;
+
+  const leftMin = searchMinDepth(root.left, depth);
+  const rightMin = searchMinDepth(root.right, depth);
+
+  const min = Math.min(leftMin, rightMin);
+  return min;
 };
+
+const minDepth = (root) => {
+  if (root === null) return 0;
+  const value = searchMinDepth(root, 0);
+  return value === MAX ? 1 : value;
+};
+
+// var minDepth = function(root) {
+//     let min=Infinity;
+//     function DFS(node){
+//         if(!node) return 0;
+        
+//         console.log("node.val:",node.val);
+//         console.log("node.left:",node.left,"node.right:",node.right);
+//         if(!node.left) return DFS(node.right)+1;
+//         if(!node.right) return DFS(node.left)+1;
+        
+//         const leftDepth=DFS(node.left);
+//         const rightDepth=DFS(node.right);
+        
+//         console.log("leftDepth:",leftDepth,"rightDepth:",rightDepth);
+//         return 1+Math.min(leftDepth,rightDepth);
+//     }
+//     return DFS(root);
+// };
