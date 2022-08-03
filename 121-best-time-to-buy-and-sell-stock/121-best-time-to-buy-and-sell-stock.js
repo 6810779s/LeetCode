07116fd1d1939456b2a9b5a5332162dcf2ecd822
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-08-03 16:53:22 +0900

    Time: 108 ms (71.81%), Space: 51.6 MB (79.03%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,25 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let result=0;
-    let min=prices[0];
-    
-    for(let i=1;i<prices.length;i++){
-        min=Math.min(prices[i],min);
-        result=Math.max(prices[i]-min,result);
+    let max=0;
+    let left=0;
+    let right=1;
+    while(right<prices.length){
+        if(prices[left]>prices[right]){
+            left=right;
+        }else{
+            const profit=prices[right]-prices[left];
+            max=Math.max(max,profit);
+        }
+        right++;
     }
-    return result;
+    return max;
+//     let result=0;
+//     let min=prices[0];
+    
+//     for(let i=1;i<prices.length;i++){
+//         min=Math.min(prices[i],min);
+//         result=Math.max(prices[i]-min,result);
+//     }
+//     return result;
 };
