04378393afcb833b938a6b1d73a93665c7a97ea0
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-08-20 23:49:53 +0900

    Time: 202 ms (57.48%), Space: 14.1 MB (74.58%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
@@ -1,23 +1,23 @@
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         
-        stations.append([target, 0])    # regard target as a station
+        stations.append([target, 0])  
         fuel = startFuel
         cnt, prev = 0, 0
         miss = []
         
         for pos, gas in stations:
-            dis, prev = pos - prev, pos    # calculate the distance between two stations
+            dis, prev = pos - prev, pos 
             
-            if fuel < dis:  # we are running out of fuel
+            if fuel < dis: 
                 
-                while miss and fuel < dis:  # use Time machine to get some fuel we missed~
+                while miss and fuel < dis: 
                     fuel += -heapq.heappop(miss)
-                    cnt += 1    # cnt is how many times we travel back to get gas
+                    cnt += 1   
                     
-                if fuel < dis: return -1    # we have used all the gas, but still cannot get to the next station
+                if fuel < dis: return -1    
                 
             fuel -= dis
-            heapq.heappush(miss, -gas)  # we don't need the gas until we run out of all fuel
+            heapq.heappush(miss, -gas) 
             
         return cnt
